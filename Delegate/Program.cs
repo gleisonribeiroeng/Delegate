44da545a5fea0aeceb088ec3113e9a11f4969d0e
XMrGX <gleison423200@gmail.com>
2024-01-22 09:27:55 -0300

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿PrintDelegate PrintConsole = (string text) =>
+{
+    Console.WriteLine(text);
+};
+
+PrintDelegate PrintToFile = (string text) =>
+{
+    File.AppendAllText("./logs.txt", text);
+};
+
+static void ConnectToDatabase(PrintDelegate log)
+{
+    log("Inserting a new record into the database.");
+
+    log("The record got insert into the database.");
+}
+
+ConnectToDatabase(PrintToFile);
+
+delegate void PrintDelegate(string text);
